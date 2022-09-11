8cbf919312a6c2b94bcdd4c19db4dd0168fe2364
MrMelon <github@onpointcoding.net>
2022-09-11 18:15:54 +0100

    Add time detail for better precision

--- tables/bong-log.go
+++ tables/bong-log.go
@@ -3,9 +3,11 @@ package tables
 import "time"
 
 type BongLog struct {
-	Id               int64     `xorm:"pk autoincr"`
-	GuildId          string    `xorm:"guild_id"`
-	UserId           string    `xorm:"user_id"`
-	Timestamp        time.Time `xorm:"timestamp"`
-	MessageTimestamp time.Time `xorm:"message_timestamp"`
+	Id            int64     `xorm:"pk autoincr"`
+	GuildId       string    `xorm:"guild_id"`
+	UserId        string    `xorm:"user_id"`
+	Timestamp     time.Time `xorm:"timestamp"`
+	TimeDetail    int64     `xorm:"time_detail"`
+	MsgTimestamp  time.Time `xorm:"message_timestamp"`
+	MsgTimeDetail int64     `xorm:"message_time_detail"`
 }
