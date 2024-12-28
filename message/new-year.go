c79663fd6ceb9f2da7d878bce296468050878f98
MrMelon54 <github@mrmelon54.com>
2024-12-28 19:06:26 +0000

    Update leaderboard website address

--- message/new-year.go
+++ message/new-year.go
@@ -17,7 +17,7 @@ var newYearsMessage string
 func SendNewYearNotification(client bot.Client, wg *sync.WaitGroup, conf database.Guild, oldYear, newYear int) {
 	defer wg.Done()
 	builder := MakeMessageNotification("Happy New Year's", newYearsMessage, "https://twemoji.maxcdn.com/v/latest/72x72/1f386.png", 0xd4af37, oldYear, newYear, time.Date(newYear, time.January, 1, 0, 0, 0, 0, time.UTC))
-	a := []discord.InteractiveComponent{discord.NewLinkButton("Big Ben Archive", "https://bigben.mrmelon54.com").WithEmoji(discord.ComponentEmoji{Name: ""})}
+	a := []discord.InteractiveComponent{discord.NewLinkButton("Big Ben Archive", "https://bigben.discord-plays.xyz").WithEmoji(discord.ComponentEmoji{Name: ""})}
 	builder.SetContainerComponents(discord.NewActionRow(a...))
 	_, err := client.Rest().CreateMessage(conf.BongChannelID, builder.Build())
 	if err != nil {
